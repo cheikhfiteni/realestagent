31b33aeb7f504ee59ac67364bd3d98ef5240a2fa
cheikhfiteni <cheikhfiteni@gmail.com>
2024-12-14 16:50:01 -0800

    fix: remove import

--- app/models/models.py
+++ app/models/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
-from app.config import DATABASE_URL, DB_PATH
+from app.config import DATABASE_URL
 
 Base = declarative_base()
 class Listing(Base):
