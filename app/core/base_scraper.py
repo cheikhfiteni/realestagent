558596da96ee1a063199c6b52436b04313fb90d1
cheikhfiteni <cheikhfiteni@gmail.com>
2025-05-07 08:32:36 -0700

    remove no sandbox to prevent r/w on selenium

--- app/core/base_scraper.py
+++ app/core/base_scraper.py
@@ -41,7 +41,6 @@ class DriverManager:
                 
                 chrome_options = Options()
                 chrome_options.add_argument('--headless=new')
-                chrome_options.add_argument('--no-sandbox')
                 chrome_options.add_argument('--disable-dev-shm-usage')
                 chrome_options.add_argument('--disable-gpu')
                 chrome_options.add_argument('--disable-extensions')
